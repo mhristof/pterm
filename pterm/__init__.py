9530ce393d8d53474bf24261e5488198d82d5ea5
Mike Christofilopoulos <mhristof@gmail.com>
2020-02-18 21:07:03 +0000

    bug fix id_rsa password

--- pterm/__init__.py
+++ pterm/__init__.py
@@ -123,7 +123,7 @@ def triggers():
         {
             "partial": True,
             "parameter": "id_rsa",
-            "regex": f"^Enter passphrase for {os.getenv('HOME')}/.ssh/id_rsa",
+            "regex": f"^Enter passphrase for key {os.getenv('HOME')}/.ssh/id_rsa",
             "action": "PasswordTrigger"
         },
         {
